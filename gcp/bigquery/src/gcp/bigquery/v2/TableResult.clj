36c2c54049e19153dd4916bb39b47cae8f3fe3ab
pat killean <pat.killean@gmail.com>
2025-03-02 13:00:47 -0400

    bq.Range, tune-up bq.TableResult

--- gcp/bigquery/src/gcp/bigquery/v2/TableResult.clj
+++ gcp/bigquery/src/gcp/bigquery/v2/TableResult.clj
@@ -1,50 +1,49 @@
 (ns gcp.bigquery.v2.TableResult
   (:require [gcp.bigquery.v2.Field :as Field]
+            [gcp.bigquery.v2.Range :as Range]
+            [gcp.global :as g]
             [jsonista.core :as j])
   (:import (com.google.cloud.bigquery FieldValue TableResult)
            (java.time LocalDate LocalDateTime)
            (java.time.format DateTimeFormatter)))
 
-;FieldValue$Attribute/RANGE
-;FieldValue$Attribute/RECORD
-;FieldValue$Attribute/REPEATED
-;FieldValue$Attribute/PRIMITIVE
+(def date-formatter     (DateTimeFormatter/ofPattern "yyyy-MM-dd"))
+(def datetime-formatter (DateTimeFormatter/ofPattern "yyyy-MM-dd'T'HH:mm:ss"))
 
-;; TODO periodDuration, range, record
+(defn field-value-to-edn
+  [field-type ^FieldValue field-value]
+  (g/strict! :gcp/bigquery.LegacySQLTypeName field-type)
+  (when-not (.isNull field-value)
+    (case (.name (.getAttribute field-value))
+      "RECORD" (.getValue field-value) ;; without schema, falls on user to make sense of this
+      "RANGE" (Range/to-edn (.getRangeValue field-value))
+      "REPEATED" (mapv (partial field-value-to-edn field-type) (.getRepeatedValue field-value))
+      "PRIMITIVE"
+      (case field-type
+        "BOOLEAN"   (.getBooleanValue field-value)
+        "STRING"    (.getStringValue field-value)
+        "INTEGER"   (.getLongValue field-value)
+        "FLOAT"     (.getDoubleValue field-value)
+        "TIMESTAMP" (.getTimestampInstant field-value)
+        "NUMERIC"   (.getNumericValue field-value)
+        "DATE"      (LocalDate/parse (.getValue field-value) date-formatter)
+        "DATETIME"  (LocalDateTime/parse (.getValue field-value) datetime-formatter)
+        "JSON"      (j/read-value (.getValue field-value) j/keyword-keys-object-mapper)
+        "BYTES"     (.getBytesValue field-value)
+        "GEOGRAPHY" (.getStringValue field-value)
+        "INTERVAL"  (.getPeriodDuration field-value) ;org.threeten.extra.PeriodDuration
+        (throw (ex-info (str "unimplemented PRIMITIVE '" field-type "'") {:field-value field-value
+                                                                          :field-type  field-type}))))))
 
-(defn- parse-field-value
-  [{:as field} ^FieldValue fieldValue]
-  (when-not (.isNull fieldValue)
-    (let [attribute (.name (.getAttribute fieldValue))]
-      (if (= attribute "PRIMITIVE")
-        (case (:type field)
-          "STRING"    (.getStringValue fieldValue)
-          "FLOAT"     (.getDoubleValue fieldValue)
-          "TIMESTAMP" (.getTimestampInstant fieldValue)
-          "INTEGER"   (.getLongValue fieldValue)
-          "BOOLEAN"   (.getBooleanValue fieldValue)
-          "NUMERIC"   (.getNumericValue fieldValue)
-          "DATE"      (LocalDate/parse (.getValue fieldValue) (DateTimeFormatter/ofPattern "yyyy-MM-dd"))
-          "DATETIME"  (LocalDateTime/parse (.getValue fieldValue) (DateTimeFormatter/ofPattern "yyyy-MM-dd'T'HH:mm:ss"))
-          "JSON"      (j/read-value (.getValue fieldValue) j/keyword-keys-object-mapper)
-          (throw (ex-info (str "unimplemented field type " (:type field)) {:field field :fieldValue field})))
-        (if (= attribute "REPEATED")
-          (case (:type field)
-            "STRING" (mapv #(.getStringValue %) (.getRepeatedValue fieldValue))
-            (throw (Exception. (str "unimplemented REPEATED type '" (:type field) "'"))))
-          (throw (Exception. (str "unimplemented attribute '" attribute "'"))))))))
-
-(defn- parse-row
-  [{:as field} fieldValue]
-  [(keyword (:name field)) (parse-field-value field fieldValue)])
+(defn- parse-row [{:as field} fieldValue]
+  [(keyword (:name field)) (field-value-to-edn (:type field) fieldValue)])
 
 (defn to-edn [^TableResult res]
   (when (.hasNextPage res)
     (throw (Exception. "unimplemented")))
   (if-let [schema (.getSchema res)]
     (let [columns    (mapv Field/to-edn (.getFields schema))
-          row-parser (fn [row]
-                       (into {} (map parse-row columns row)))]
+          row-parser (fn [row] (into {} (map parse-row columns row)))]
       (mapv row-parser (.iterateAll res)))
     (if (zero? (.getTotalRows res))
       []
